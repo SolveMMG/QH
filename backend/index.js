cc211c421cc7aa387506bf978dee2290a5ed22e7
Michael Gacheru <mikachupichu@gmail.com>
2025-05-16 14:24:49 +0300

    Deploying frontend

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: ['https://your-vercel-app-url.vercel.app'],
+  origin: ['https://qh-ashen.vercel.app/'],
   credentials: true, // if you're using cookies/auth
 }));
 app.use(express.json());
