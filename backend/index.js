355723c145ceecee38b39b8313264fa56ed61398
Michael Gacheru <mikachupichu@gmail.com>
2025-05-17 16:23:53 +0300

    Adding a different frontend link

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: ['https://qh-ashen.vercel.app'],
+  origin: ['https://qh-i1um.vercel.app'],
   credentials: true, // if you're using cookies/auth
 }));
 app.use(express.json());
